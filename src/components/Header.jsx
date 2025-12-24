79d2a0a508bba821cc0cac3371f01c623e77235b
Deepak S <136345411+depak7@users.noreply.github.com>
2025-12-24 19:01:58 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -63,7 +63,7 @@ const Header = () => {
               </svg>
             </a>
             <a 
-              href="https://drive.google.com/uc?export=download&id=1V8S8JtXEwqbAaWeD-tasHvCpaJHYuzwB" 
+              href="https://drive.google.com/file/d/17spUIz5PlOE9Dg_R_OOqB5zV8BdPgN8M" 
               target="_blank"
               rel="noopener noreferrer"
               className="w-9 h-9 sm:w-10 sm:h-10 flex items-center justify-center hover:text-gray-400 transition-colors"
